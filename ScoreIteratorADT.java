8f4864b865d86b199ef8b3c1fce97439f6dfb2f7
tessmcchesney <tmcchesney@wisc.edu>
2017-02-11 19:09:39 -0600

    Update ScoreIteratorADT.java

--- ScoreIteratorADT.java
+++ ScoreIteratorADT.java
@@ -1,4 +1,30 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2017 
+// PROJECT:          p1
+// FILE:             ScoreIteratorADT.java
+//
+// TEAM:    Team 54, null
+// Authors: (Be sure to check if programming teams are allowed)
+// Author1: (name1,email1,netID1,lecture number1)
+// Author2: (name2,email2,netID2,lecture number2)
+//
+// ---------------- OTHER ASSISTANCE CREDITS 
+// Persons: Identify persons by name, relationship to you, and email. 
+// Describe in detail the the ideas and help they provided. 
+// 
+// Online sources: avoid web searches to solve your problems, but if you do 
+// search, be sure to include Web URLs and description of 
+// of any information you find. 
+////////////////////////////////////////////////////////////////////////////
 
+/**
+ * This interface is implemented by the ScoreIterator in order to point
+ * at different Scores in a ScoreList.
+ *
+ * <p>Bugs: none
+ *
+ * @author (your name)
+ */
 public interface ScoreIteratorADT {
 	boolean hasNext();
 	Score next();
