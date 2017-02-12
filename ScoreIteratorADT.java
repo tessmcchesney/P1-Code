9128be72aaf590450b0ad98efd0b7453d0c242c3
tessmcchesney <tmcchesney@wisc.edu>
2017-02-11 19:27:19 -0600

    Update ScoreIteratorADT.java

--- ScoreIteratorADT.java
+++ ScoreIteratorADT.java
@@ -1,4 +1,11 @@
-
+/**
+ * This interface is implemented by the ScoreIterator in order
+ * to point to Scores within a ScoreList
+ *
+ * <p>Bugs: none
+ *
+ * @author (your name)
+ */
 public interface ScoreIteratorADT {
 	boolean hasNext();
 	Score next();
